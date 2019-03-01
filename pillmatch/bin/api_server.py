58623b5fdba6892df949888adb79ae1882faf9ed
Brad Arsenault <brad@electricbrain.io>
2019-03-01 18:17:55 -0500

    Fixed handling of static folders in API application.

--- pillmatch/bin/api_server.py
+++ pillmatch/bin/api_server.py
@@ -2,7 +2,7 @@ from pillmatch.matcher import Matcher
 from pillmatch.utilities import merge
 from pillmatch.pill_db import PillDB
 from flask import Flask
-from flask import request
+from flask import request, send_from_directory
 import json
 import numpy
 import os
@@ -10,8 +10,10 @@ import skimage.io
 import tempfile
 import io
 from flask_cors import CORS
-app = Flask(__name__)
+clientFolder = '/home/bradley/eb-pill-match/pillmatch/client'
+app = Flask(__name__, static_folder=clientFolder)
 CORS(app)
+
 
 
 with open('default_parameters.json', 'rt') as file:
@@ -77,6 +79,52 @@ def imageMatch():
 @app.route("/PillCoordinates", methods=['GET'])
 def pillCoordinates():
     return json.dumps({"pills": db.getPills()[-100:]})
+
+
+@app.route('/static/<path:path>', methods=['GET'])
+def send_client_static(path):
+    return send_from_directory(f'{clientFolder}/static', path)
+
+@app.route('/themes/<path:path>', methods=['GET'])
+def send_client_themes(path):
+    return send_from_directory(f'{clientFolder}/themes', path)
+
+@app.route('/locales/<path:path>', methods=['GET'])
+def send_client_locales(path):
+    return send_from_directory(f'{clientFolder}/locales', path)
+
+@app.route('/img/<path:path>', methods=['GET'])
+def send_client_img(path):
+    return send_from_directory(f'{clientFolder}/img', path)
+
+@app.route('/', methods=['GET'])
+def root():
+    return app.send_static_file(f'index.html')
+
+@app.route('/index.html', methods=['GET'])
+def root3():
+    return app.send_static_file(f'index.html')
+
+@app.route('/asset-manifest.json', methods=['GET'])
+def asset():
+    return app.send_static_file(f'asset-manifest.json')
+
+@app.route('/favicon.ico', methods=['GET'])
+def favicon():
+    return app.send_static_file(f'favicon.ico')
+
+@app.route('/manifest.json', methods=['GET'])
+def manifest():
+    return app.send_static_file(f'manifest.json')
+
+@app.route('/service-worker.js', methods=['GET'])
+def servieworker():
+    return app.send_static_file(f'service-worker.js', methods=['GET'])
+
+@app.route('/precache-manifest.95cd47c065c9cb29362506bd58b89271', methods=['GET'])
+def precache():
+    return app.send_static_file(f'precache-manifest.95cd47c065c9cb29362506bd58b89271')
+
 
 def main():
     app.run(host="192.168.1.17")
