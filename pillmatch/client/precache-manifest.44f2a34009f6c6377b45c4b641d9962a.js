e375876811922adb36cfb22dc26128a2c678e933
Brad Arsenault <brad@electricbrain.io>
2019-03-01 18:20:49 -0500

    Added in prebuilt client files to repo.

---
+++ pillmatch/client/precache-manifest.44f2a34009f6c6377b45c4b641d9962a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "./static/media/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "5f91dd2406b32e0d2e35",
+    "url": "./static/css/main.46b108b1.chunk.css"
+  },
+  {
+    "revision": "7a012e10df6e1864b4f2",
+    "url": "./static/js/1.7a012e10.chunk.js"
+  },
+  {
+    "revision": "a13c4702803d3460c502",
+    "url": "./static/js/2.a13c4702.chunk.js"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "./static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "09b4899265dd8278009c",
+    "url": "./static/js/3.09b48992.chunk.js"
+  },
+  {
+    "revision": "b0313b8100719e4c1efe",
+    "url": "./static/js/4.b0313b81.chunk.js"
+  },
+  {
+    "revision": "471a08cb6282c8f55572",
+    "url": "./static/js/runtime~main.471a08cb.js"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "./static/media/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "./static/media/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "./static/media/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "./static/media/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "42f9fd6acee87559ac0d6a33488db65e",
+    "url": "./static/media/fa-brands-400.42f9fd6a.svg"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "./static/media/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "5f91dd2406b32e0d2e35",
+    "url": "./static/js/main.5f91dd24.chunk.js"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "./static/media/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "./static/media/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "0c41971339b9fc5b1cefb0abad1e2e69",
+    "url": "./static/media/fa-regular-400.0c419713.svg"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "./static/media/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "./static/media/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "./static/media/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "./static/media/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "4478b4d7022cad174e4c04246fe622ef",
+    "url": "./static/media/fa-solid-900.4478b4d7.svg"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "./static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "./static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "./static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "./static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "09b4899265dd8278009c",
+    "url": "./static/css/3.cd01d33c.chunk.css"
+  },
+  {
+    "revision": "22f570da6663d93a0b186c74639a835a",
+    "url": "./index.html"
+  }
+];
